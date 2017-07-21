69b8dcf58b1d0523792d6f203946da0bc637da45
Romain Jouffret <r.nassty@gmail.com>
2017-07-21 17:50:51 +0200

    ADD - jaccard et shingles

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import sys
 from pathlib import Path
 
+# ===== SHINGLING ======
+#
+SHINGLE_SIZE = 5
+
+def get_shingles(f, size):
+    shingles = set()
+    buf = f # read entire file
+    for i in range(0, len(buf)-size+1):
+        yield buf[i:i+size]
+
+# ===== JACCARD ======
+#
+def jaccard(set1, set2):
+    x = len(set1.intersection(set2))
+    y = len(set1.union(set2))
+    return x / float(y)
 
 # ===== MAIN ======
 #
@@ -9,8 +25,13 @@ def main():
     f = open('items.txt', 'r')
     #print(''.join(f.readline()))
     texts = f.readlines()
-    print(size(texts))
+    for t in texts:
+        shingles1 = set(get_shingles(t, size=SHINGLE_SIZE))
+        shingles2 = set(get_shingles(t, size=SHINGLE_SIZE))
+
+    print(jaccard(shingles1, shingles2))
     print("\n*+*+*+*+*+* END *+*+*+*+*+*\n")
+    # ADD TIMER
 
 # ===== EXECUTE =====
 #
